71567e9a94094d477f3f09868197946f19e05a5d
stefan0re <stremke@proton.me>
2025-05-01 13:36:42 +0200

    Adjusted driver

--- assembly_examples/task_3/driver.cpp
+++ assembly_examples/task_3/driver.cpp
@@ -182,7 +182,7 @@ void bench_mm(uint64_t i_reps,
 }
 
 int main() {
-    bench_mm(150000000,
+    bench_mm(200000000,
              16,
              6,
              1,
@@ -191,7 +191,7 @@ int main() {
              16,
              matmul_16_6_1);
 
-    bench_mm(10000000,
+    bench_mm(15000000,
              16,
              6,
              64,
@@ -200,7 +200,7 @@ int main() {
              16,
              matmul_16_6_64);
 
-    bench_mm(2000000,
+    bench_mm(3000000,
              64,
              6,
              64,
@@ -209,7 +209,7 @@ int main() {
              64,
              matmul_64_6_64);
 
-    bench_mm(250000,
+    bench_mm(300000,
              64,
              48,
              64,
