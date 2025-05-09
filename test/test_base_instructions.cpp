f36e2cb8ce941c8ed6198095444919d3a3028c8d
Phillip Rothenbeck <philliprothenbeck@gmail.com>
2025-05-09 09:17:16 +0200

    restore old bas instr test file

--- test/test_base_instructions.cpp
+++ test/test_base_instructions.cpp
@@ -17,24 +17,6 @@ int test_base_br_cbnz() {
     return 0;
 }
 
-int test_neon_fmla_element() {
-    using namespace mini_jit::instructions;
-
-    std::cout << "Testing Neon FMLA (by element) instruction generation ... ";
-    uint32_t instruction = InstGen::neon_fmla_element(InstGen::simd_fp_t::v8,
-                                                      InstGen::simd_fp_t::v8,
-                                                      InstGen::simd_fp_t::v9,
-                                                      InstGen::element_spec_t::s_1);
-    uint32_t expected = 0x5FD91108;
-
-    if (instruction != expected) {
-        std::cout << "Failed" << std::endl;
-        return -1;
-    }
-    std::cout << "Done" << std::endl;
-    return 0;
-}
-
 int main() {
     int result = 0;
 
