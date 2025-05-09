5b32dc87f57152edf3be00873af065d9d3d9277c
PhillipRothenbeck <73098807+FlipediFlop@users.noreply.github.com>
2025-05-09 13:40:08 +0200

    use seperate tests_util file

--- test/test_neon_instructions.cpp
+++ test/test_neon_instructions.cpp
@@ -1,23 +1,19 @@
-#include <iostream>
-
 #include "../src/mini_jit/instructions/instructions.h"
+#include "./test_utils.h"
 
 int test_neon_fmla_element() {
     using namespace mini_jit::instructions;
+    uint32_t mc1 = InstGen::neon_fmla_element(InstGen::simd_fp_t::v8,
+                                              InstGen::simd_fp_t::v8,
+                                              InstGen::simd_fp_t::v9,
+                                              InstGen::element_spec_t::S4_1);
+    uint32_t mc2 = test_utils::as("fmla v8.4s, v8.4s, v9.s[1]");
 
-    std::cout << "Testing Neon FMLA (by element) instruction generation ... ";
-    uint32_t instruction = InstGen::neon_fmla_element(InstGen::simd_fp_t::v8,
-                                                      InstGen::simd_fp_t::v8,
-                                                      InstGen::simd_fp_t::v9,
-                                                      InstGen::element_spec_t::s_1);
-    uint32_t expected = 0x5FD91108;
-
-    if (instruction != expected) {
-        std::cout << "Failed" << std::endl;
-        return -1;
-    }
-    std::cout << "Done" << std::endl;
-    return 0;
+    bool match = (mc1 == mc2);
+    std::cout << "stp w1, w2, [x3], #0: "
+              << test_utils::get_binary(mc1) << " | " << test_utils::get_binary(mc2) << " : "
+              << std::boolalpha << match << std::endl;
+    return match ? 0 : -1;
 }
 
 int main() {
