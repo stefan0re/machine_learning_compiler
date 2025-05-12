5a9306e595e64027a0634b1ff4f852ea6619e3f4
gh-tom-schmidt <tom.schmidt-business@protonmail.com>
2025-05-12 09:46:06 +0200

    first working version

--- src/mini_jit/generator/util.cpp
+++ src/mini_jit/generator/util.cpp
@@ -133,26 +133,26 @@ namespace mini_jit::generator {
         //
 
         // count how many vectors are in use
-        reg_count = 0;
+        int C_reg_count = 0;
+        int B_reg_count = -1;
 
         // total number of elements needed to load
         count = kernelsize.M;
         rem = count % 4;
 
-        // for each register holding 4 values from N
-        for (int n = 0; n < B_quads; n++) {
-            // for each value in that register (vector)
-            for (int j = 0; j < 4; j++) {
-                // for each quad m
-                for (int m = 0; m < A_quads; m++) {
-                    // mulitply four elements with one scalar (4s)
-                    m_kernel.add_instr(
-                        InstGen::neon_fmla_element(static_cast<InstGen::simd_fp_t>(++reg_count),
-                                                   static_cast<InstGen::simd_fp_t>(n * j * A_quads + m + i_used_vector_reg_count),
-                                                   static_cast<InstGen::simd_fp_t>(n + regs_after_A),
-                                                   // acces the correct index, because the s version starts at enum 4
-                                                   s4_values[j % 4]));
-                }
+        // for each col
+        for (int j = 0; j < kernelsize.N; j++) {
+            // after for values, use the next register for values in B
+            (j % 4 == 0) ? ++B_reg_count : 0;
+
+            // for each row with each quad = (4s) + 1 for remainder
+            for (int i = 0; i < A_quads + 1; i++) {
+                // mulitply four elements with one scalar (4s)
+                m_kernel.add_instr(
+                    InstGen::neon_fmla_element(static_cast<InstGen::simd_fp_t>(C_reg_count++),
+                                               static_cast<InstGen::simd_fp_t>(i + i_used_vector_reg_count),
+                                               static_cast<InstGen::simd_fp_t>(B_reg_count + regs_after_A),
+                                               s4_values[j % 4]));
             }
         }
 
@@ -168,44 +168,49 @@ namespace mini_jit::generator {
         int32_t reg_count = 0;
 
         // total number of elements needed to load
-        int count = kernelsize.M * kernelsize.N;
+        int count = kernelsize.M;
         int quads = count / 4;
         int rem = count % 4;
 
-        // main quad (4s) loop
-        for (; reg_count < quads; reg_count++) {
-            // load four elements at once (4s)
-            m_kernel.add_instr(
-                InstGen::neon_ld1_no_offset(
-                    static_cast<InstGen::simd_fp_t>(reg_count),
-                    Util::WORKING_ADDRESS_C_REG,
-                    InstGen::vector_count_t::vc4));
+        // for each col
+        for (int j = 0; j < kernelsize.N; j++) {
+            // for each row with each quad = (4s)
+            for (int i = 0; i < quads; i++) {
+                // load four elements at once (4s)
+                m_kernel.add_instr(
+                    InstGen::neon_ld1_no_offset(
+                        static_cast<InstGen::simd_fp_t>(reg_count++),
+                        Util::WORKING_ADDRESS_C_REG,
+                        InstGen::vector_count_t::vc4));
 
-            // advance the base pointer by 4 elements
-            m_kernel.add_instr(
-                InstGen::base_add_imm(
-                    Util::WORKING_ADDRESS_C_REG,
-                    Util::WORKING_ADDRESS_C_REG,
-                    4,
-                    /*no flags*/ 0));
-        }
+                // advance the base pointer by 4 elements
+                m_kernel.add_instr(
+                    InstGen::base_add_imm(
+                        Util::WORKING_ADDRESS_C_REG,
+                        Util::WORKING_ADDRESS_C_REG,
+                        4,
+                        /*no flags*/ 0));
+            }
 
-        // remainder
-        for (int i = 0; i < rem; i++) {
-            // load one element at a time (.s[N])
-            m_kernel.add_instr(
-                InstGen::neon_ld1_scalar_index(
-                    static_cast<InstGen::simd_fp_t>(reg_count),
-                    Util::WORKING_ADDRESS_C_REG,
-                    i));
+            // remainder
+            for (int i = 0; i < rem; i++) {
+                // load one element at a time (.s[N])
+                m_kernel.add_instr(
+                    InstGen::neon_ld1_scalar_index(
+                        static_cast<InstGen::simd_fp_t>(reg_count),
+                        Util::WORKING_ADDRESS_C_REG,
+                        i));
 
-            // advance the base pointer by 1 elements
-            m_kernel.add_instr(
-                InstGen::base_add_imm(
-                    Util::WORKING_ADDRESS_C_REG,
-                    Util::WORKING_ADDRESS_C_REG,
-                    1,
-                    /*no flags*/ 0));
+                // advance the base pointer by 1 elements
+                m_kernel.add_instr(
+                    InstGen::base_add_imm(
+                        Util::WORKING_ADDRESS_C_REG,
+                        Util::WORKING_ADDRESS_C_REG,
+                        1,
+                        /*no flags*/ 0));
+            }
+
+            (0 < rem) ? ++reg_count : reg_count;
         }
 
         // DEBUG: write out / reset for debugging
@@ -213,7 +218,7 @@ namespace mini_jit::generator {
         m_kernel.force_clear();
 
         // if there was a remainder, another register is used
-        return (0 < rem) ? ++reg_count : reg_count;
+        return reg_count;
     }
 
     void Util::gen_c_store(Util::KernelSize kernelsize) {
@@ -221,44 +226,49 @@ namespace mini_jit::generator {
         int32_t reg_count = 0;
 
         // total number of elements needed to load
-        int count = kernelsize.M * kernelsize.N;
+        int count = kernelsize.M;
         int quads = count / 4;
         int rem = count % 4;
 
-        // main quad (4s) loop
-        for (; reg_count < quads; reg_count++) {
-            // load four elements at once (4s)
-            m_kernel.add_instr(
-                InstGen::neon_st1_no_offset(
-                    static_cast<InstGen::simd_fp_t>(reg_count),
-                    Util::WORKING_ADDRESS_C_REG,
-                    InstGen::vector_count_t::vc4));
+        // for each col
+        for (int j = 0; j < kernelsize.N; j++) {
+            // for each row with each quad = (4s)
+            for (int i = 0; i < quads; i++) {
+                // load four elements at once (4s)
+                m_kernel.add_instr(
+                    InstGen::neon_st1_no_offset(
+                        static_cast<InstGen::simd_fp_t>(reg_count++),
+                        Util::WORKING_ADDRESS_C_REG,
+                        InstGen::vector_count_t::vc4));
 
-            // advance the base pointer by 4 elements
-            m_kernel.add_instr(
-                InstGen::base_add_imm(
-                    Util::WORKING_ADDRESS_C_REG,
-                    Util::WORKING_ADDRESS_C_REG,
-                    4,
-                    /*no flags*/ 0));
-        }
+                // advance the base pointer by 4 elements
+                m_kernel.add_instr(
+                    InstGen::base_add_imm(
+                        Util::WORKING_ADDRESS_C_REG,
+                        Util::WORKING_ADDRESS_C_REG,
+                        4,
+                        /*no flags*/ 0));
+            }
 
-        // remainder
-        for (int i = 0; i < rem; i++) {
-            // load one element at a time (.s[N])
-            m_kernel.add_instr(
-                InstGen::neon_st1_scalar_index(
-                    static_cast<InstGen::simd_fp_t>(reg_count),
-                    Util::WORKING_ADDRESS_C_REG,
-                    i));
+            // remainder
+            for (int i = 0; i < rem; i++) {
+                // load one element at a time (.s[N])
+                m_kernel.add_instr(
+                    InstGen::neon_st1_scalar_index(
+                        static_cast<InstGen::simd_fp_t>(reg_count),
+                        Util::WORKING_ADDRESS_C_REG,
+                        i));
 
-            // advance the base pointer by 1 elements
-            m_kernel.add_instr(
-                InstGen::base_add_imm(
-                    Util::WORKING_ADDRESS_C_REG,
-                    Util::WORKING_ADDRESS_C_REG,
-                    1,
-                    /*no flags*/ 0));
+                // advance the base pointer by 1 elements
+                m_kernel.add_instr(
+                    InstGen::base_add_imm(
+                        Util::WORKING_ADDRESS_C_REG,
+                        Util::WORKING_ADDRESS_C_REG,
+                        1,
+                        /*no flags*/ 0));
+            }
+
+            (0 < rem) ? ++reg_count : reg_count;
         }
 
         // DEBUG: write out / reset for debugging
