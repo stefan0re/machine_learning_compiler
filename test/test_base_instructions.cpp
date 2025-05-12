c1bd5b4959629e36948b19441649d625f77254a6
Phillip Rothenbeck <philliprothenbeck@gmail.com>
2025-05-12 15:37:36 +0200

    is_correct -> instr_is_correct

--- test/test_base_instructions.cpp
+++ test/test_base_instructions.cpp
@@ -17,77 +17,77 @@ int test_base_ldp() {
     uint32_t mc1 = l_gen.base_ldp(gpr_t::w1, gpr_t::w2, gpr_t::x3, 0);
     string call = "ldp w1, w2, [x3], #0";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_stp() {
     uint32_t mc1 = l_gen.base_stp(gpr_t::w1, gpr_t::w2, gpr_t::x3, 0);
     string call = "stp w1, w2, [x3], #0";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_mov_imm() {
     uint32_t mc1 = l_gen.base_mov_imm(gpr_t::w1, 1, 0);
     string call = "mov w1, #1";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_mov_register() {
     uint32_t mc1 = l_gen.base_mov_register(gpr_t::w1, gpr_t::w2);
     string call = "mov w1, w2";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_add_imm() {
     uint32_t mc1 = l_gen.base_add_imm(gpr_t::w1, gpr_t::w2, 1, 0);
     string call = "add w1, w2, #1";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_add_shifted_register() {
     uint32_t mc1 = l_gen.base_add_shifted_register(gpr_t::w1, gpr_t::w2, gpr_t::w3, 0, 0);
     string call = "add w1, w2, w3";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_sub_imm() {
     uint32_t mc1 = l_gen.base_sub_imm(gpr_t::w1, gpr_t::w2, 1, 0);
     string call = "sub w1, w2, #1";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_sub_shifted_register() {
     uint32_t mc1 = l_gen.base_sub_shifted_register(gpr_t::w1, gpr_t::w2, gpr_t::w3, 0, 0);
     string call = "sub w1, w2, w3";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_lsl_imm() {
     uint32_t mc1 = l_gen.base_lsl_imm(gpr_t::w1, gpr_t::w2, 0);
     string call = "lsl w1, w2, #0";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_lsl_register() {
     uint32_t mc1 = l_gen.base_lsl_register(gpr_t::w1, gpr_t::w2, gpr_t::w3);
     string call = "lsl w1, w2, w3";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int test_base_br_cbnz() {
     uint32_t mc1 = l_gen.base_br_cbnz(gpr_t::w1, 1);
     string call = "cbnz w1, 0x00000004";
     uint32_t mc2 = test_utils::as(call);
-    return test_utils::is_correct(call, mc1, mc2);
+    return test_utils::instr_is_correct(call, mc1, mc2);
 }
 
 int main() {
