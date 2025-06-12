b0ba1cd1262f1f9045c74b141f4a022b45794251
PhillipRothenbeck <philliprothenbeck@gmail.com>
2025-06-12 17:15:22 +0000

    add structures for storing tensor operations

--- src/einsum/trees/einsum_trees.h
+++ src/einsum/trees/einsum_trees.h
@@ -5,30 +5,55 @@
 #include <string>
 #include <vector>
 
+#include "../backend/TensorOperation.h"
+
 namespace einsum {
     namespace trees {
+        struct OpSteps {
+            struct OpStep {
+                uint32_t in_ten_left;
+                std::vector<uint32_t> in_ten_left_notation;
+                uint32_t in_ten_right;
+                std::vector<uint32_t> in_ten_right_notation;
+                uint32_t out_ten;
+                std::vector<uint32_t> out_ten_notation;
+            };
+            std::vector<einsum::backend::TensorOperation> step_list;
+            std::vector<OpStep> tensor_order;
+        };
+
         class EinsumTree;
-    }
+    }  // namespace trees
 }  // namespace einsum
 
 class einsum::trees::EinsumTree {
    private:
+    enum class node_t : uint32_t {
+        leaf = 0,
+        permutation = 1,
+        contraction = 2,
+    };
+
     struct TreeNode {
+        int32_t id;
         std::vector<uint32_t> notation;
-        bool is_leaf;
+        node_t node_type;
         TreeNode* parent;
         TreeNode* left_child;
         TreeNode* right_child;
     };
 
     TreeNode* root = nullptr;
-    std::vector<uint32_t> id_dims = {};
     uint32_t size = 0;
+    std::vector<uint32_t> id_dims = {};
+    std::vector<int32_t> leaf_ids = {};
+
     void printNode(TreeNode* node, const std::string& prefix, bool isLast);
+    OpSteps::OpStep lowerNode(TreeNode* node, OpSteps& lowered);
 
    public:
     EinsumTree(std::string str_repr, std::vector<uint32_t> id_dims);
-    void execute();
+    OpSteps lower();
 
     void print();
 };
