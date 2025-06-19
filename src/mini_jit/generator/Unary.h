841944b7af900a4886589db615282af3fb3ccbc4
stefan0re <stremke@proton.me>
2025-06-19 18:30:27 +0200

    Adjusted unary transpose op

--- src/mini_jit/generator/Unary.h
+++ src/mini_jit/generator/Unary.h
@@ -14,10 +14,6 @@ class mini_jit::generator::Unary {
    private:
     static mini_jit::backend::Kernel m_kernel;
 
-    static int32_t gen_unary_zero(mini_jit::generator::Util::KernelSize kernelsize);
-    static int32_t gen_unary_relu(mini_jit::generator::Util::KernelSize kernelsize);
-    static void gen_unary_transpose(uint32_t m, uint32_t n);
-
    public:
     int32_t fops = 0;
 
@@ -31,7 +27,8 @@ class mini_jit::generator::Unary {
     enum class ptype_t : uint32_t {
         zero = 0,
         identity = 1,
-        relu = 2
+        relu = 2,
+        trans = 3
     };
 
     /// error codes
@@ -39,17 +36,11 @@ class mini_jit::generator::Unary {
         success = 0
     };
 
-    /**
-     * @brief Get kernel sizes for sub-matrices.
-     * @param m            Number of rows in A and B.
-     * @param n            Number of columns in A and B.
-     * @param kernel_sizes size for each kernel for the sub-matrices.
-     * @return error_t::success on success, another error_t value otherwise.
-     **/
-    static error_t get_kernel_sizes(uint32_t m,
-                                    uint32_t n,
-                                    Util::KernelSizes& kernel_sizes,
-                                    bool only_square = false);
+    void gen_transpose_micro( uint32_t i_m,
+                              uint32_t i_n);
+
+    void gen_transpose( uint32_t i_m,
+                               uint32_t i_n);
 
     /**
      * @brief Generate a kernel for a unary primitive.
@@ -62,7 +53,6 @@ class mini_jit::generator::Unary {
      **/
     error_t generate(uint32_t m,
                      uint32_t n,
-                     uint32_t trans_b,
                      dtype_t dtype,
                      ptype_t ptype);
 
