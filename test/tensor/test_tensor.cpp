4bac9908b751f59ba671cb826c5290f6d203c514
stefan0re <stremke@proton.me>
2025-06-20 21:07:55 +0200

    Fixed test by removing tensor tests

--- test/tensor/test_tensor.cpp
+++ test/tensor/test_tensor.cpp
@@ -6,22 +6,3 @@
 
 #include "../../src/tensor/tensor.cpp"
 #include "../../src/tensor/tensor.h"
-
-TEST_CASE("Tensor.info()", "[tensor][info]") {
-    Tensor tensor = Tensor(3, 4, 5);
-
-    INFO(tensor.info_str());
-    REQUIRE(false);
-}
-
-TEST_CASE("Tensor.swap()", "[tensor][info]") {
-    Tensor tensor = Tensor(3, 4, 5);
-
-    INFO(tensor.info_str());
-
-    tensor.swap(1, 2);
-
-    INFO(tensor.info_str());
-
-    REQUIRE(false);
-}
