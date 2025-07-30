f73cae8bd9a1cdc746d3b29bb4bde57aa6056098
PhillipRothenbeck <philliprothenbeck@gmail.com>
2025-07-30 14:48:32 +0000

    add working benchmarks

--- benchmark/bench_tree.cpp
+++ benchmark/bench_tree.cpp
@@ -1,3 +1,4 @@
+#include <chrono>
 #include <iostream>
 #include <string>
 
@@ -56,22 +57,30 @@ void test_simple_str() {
  *                  8: 3
  * Notation; Node ID / First Touch | Operation Primitive | Last Touch
     └─ 0,1,2,3,4; 0 / 99 | 3 | 99
-    ├─ 7,3,4; 1 / 99 | 3 | 99
-    │  ├─ 8,4; 2 / 99 | 99 | 99
-    │  └─ 7,3,8; 3 / 99 | 99 | 99
-    └─ 0,1,2,7; 4 / 99 | 3 | 99
-        ├─ 1,2,5,7; 5 / 99 | 3 | 99
-        │  ├─ 2,6,7; 6 / 99 | 99 | 99
-        │  └─ 1,5,6; 7 / 99 | 99 | 99
-        └─ 0,5; 8 / 99 | 99 | 99
+       ├─ 7,3,4; 1 / 99 | 3 | 99
+       │  ├─ 8,4; 2 / 99 | 99 | 99
+       │  └─ 7,3,8; 3 / 99 | 99 | 99
+       └─ 0,1,2,7; 4 / 99 | 3 | 99
+           ├─ 1,2,5,7; 5 / 99 | 3 | 99
+           │  ├─ 2,6,7; 6 / 99 | 99 | 99
+           │  └─ 1,5,6; 7 / 99 | 99 | 99
+           └─ 0,5; 8 / 99 | 99 | 99
     Leaf ID's: 2 3 6 7 8
  */
 void first_example() {
     std::cout << "Running first pbtc example..." << std::endl;
     std::string str_repr = "[[8,4],[7,3,8]->[7,3,4]],[[[2,6,7],[1,5,6]->[1,2,5,7]],[0,5]->[0,1,2,7]]->[0,1,2,3,4]";
+    auto start = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < 50; i++) {
+        EinsumTree tree = EinsumTree(str_repr, {100, 72, 128, 128, 3, 71, 305, 32, 3});
+        tree.lower();
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
+    std::cout << "  Compiling first example: " << elapsed.count() << " seconds" << std::endl;
+    // double gflops = tensor_op.get_flops_count() / elapsed.count() / 1e9;
+
     EinsumTree tree = EinsumTree(str_repr, {100, 72, 128, 128, 3, 71, 305, 32, 3});
-    // tree.optimize();
-    tree.print();
     tree.lower();
 
     float* in0 = new float[3 * 3];
@@ -81,14 +90,66 @@ void first_example() {
     float* in4 = new float[100 * 71];
     float* out = new float[100 * 72 * 128 * 128 * 3];
 
+    start = std::chrono::high_resolution_clock::now();
     tree.execute({in0, in1, in2, in3, in4}, {}, out);
+    end = std::chrono::high_resolution_clock::now();
+    elapsed = end - start;
+    std::cout << "  Execution first example: " << elapsed.count() << " seconds" << std::endl;
 
+    tree.delete_tree();
     std::cout << "Finished first pbtc example..." << std::endl;
+}
+
+/**
+ * Contraction String: [[[[3,6,8,9]->[8,6,9,3]],[[2,5,7,9]->[7,5,2,9]]->[7,8,5,6,2,3]],[0,4,5,6]->[0,4,7,8,2,3]],[1,4,7,8]->[0,1,2,3]
+ * Dimension sizes: 0: 60
+ *                  1: 60,
+ *                  2: 20,
+ *                  3: 20,
+ *                  4: 8,
+ *                  5: 8,
+ *                  6: 8,
+ *                  7: 8,
+ *                  8: 8,
+ *                  9: 8
+ * Notation; Node ID / First Touch | Operation Primitive | Last Touch
+    └─ 0,1,2,3; 0 / 99 | 3 | 99
+       ├─ 0,4,7,8,2,3; 1 / 99 | 3 | 99
+       │  ├─ 7,8,5,6,2,3; 2 / 99 | 3 | 99
+       │  │  ├─ 8,6,9,3; 3 / 99 | 1 | 99
+       │  │  │  └─ 3,6,8,9; 4 / 99 | 99 | 99
+       │  │  └─ 7,5,2,9; 5 / 99 | 1 | 99
+       │  │     └─ 2,5,7,9; 6 / 99 | 99 | 99
+       │  └─ 0,4,5,6; 7 / 99 | 99 | 99
+       └─ 1,4,7,8; 8 / 99 | 99 | 99
+    Leaf ID's: 4 6 7 8
+    Bias ID's: 0 1 2
+ *
+ */
+
+void second_example() {
+    std::cout << "Running second pbtc example..." << std::endl;
+    std::string str_repr = "[[[[3,6,8,9]->[8,6,9,3]],[[2,5,7,9]->[7,5,2,9]]->[7,8,5,6,2,3]],[0,4,5,6]->[0,4,7,8,2,3]],[1,4,7,8]->[0,1,2,3]";
+    EinsumTree tree = EinsumTree(str_repr, {60, 60, 20, 20, 8, 8, 8, 8, 8, 8});
+    tree.lower();
+    tree.print();
+
+    float* in0 = new float[20 * 8 * 8 * 8];
+    float* in1 = new float[20 * 8 * 8 * 8];
+    float* in2 = new float[60 * 8 * 8 * 8];
+    float* in3 = new float[60 * 8 * 8 * 8];
+    float* out = new float[60 * 60 * 20 * 20];
+
+    std::cout << "Executing tree..." << std::endl;
+    tree.execute({in0, in1, in2, in3}, {}, out);
+
+    std::cout << "Finished second pbtc example..." << std::endl;
 }
 
 int main() {
     std::cout << "Benchmarking Einsum Strings..." << std::endl;
     test_simple_str();
     first_example();
+    // second_example();
     return EXIT_SUCCESS;
 }
